dc5ea30cd39fdb1b2b442bbbbb6973ec2b5c2e8a
xxxHolic3 <agugn.ichiruki@gmail.com>
2021-06-04 22:50:56 +0800

    First Commit

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+	let name = "Svelte";
+</script>
+
+<main>
+	<h1 class="text-5xl font-mono text-center text-light-blue-500 pt-48">{name} + WindiCSS = Awesome</h1>
+	<p class="text-2xl font-mono text-center text-orange-500 pt-5">Template generate from <a href="https://github.com/xxxHolic3/">xxxHolic3 Github.</a></p>
+</main>
+
+<style windi:preflights:global windi:safelist:global>
+</style>
